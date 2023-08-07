f716c8b353cf90977d4413fc175ba8cbc6897d72
Vladimir Egorov <24740397+Vladimire87@users.noreply.github.com>
2023-08-07 16:51:42 +0400

    scope for locale in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  resources :superheroes
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  scope "(locale)", locale: /#{I18n.available_locales.join("|")}/ do
+    resources :superheroes
+    root "superheroes#index"
+  end
 end
